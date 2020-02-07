c55ef3688f6a87e7768fb5d60bb9d3728a825332
eilehpo-Irasou <51323756+eilehpo-Irasou@users.noreply.github.com>
2020-02-07 17:29:52 +0100

    Add files via upload
    dossier pour les questions à partir de la question 7.4

---
+++ zuul-v4/__SHELL4.java
@@ -0,0 +1,10 @@
+
+public class __SHELL4 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("/home/irasou/Documents/java/zuul-v4");
+final Game game2 = (Game)__bluej_runtime_scope.get("game2");
+
+
+game2.play();
+
+}}
